bca86920a30fb82d7fe35b339bd1354f1cc0d368
MonikaPandian <panmonikmm@gmail.com>
2023-02-23 14:08:23 +0530

    video

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 __dirname = path.resolve();
-const buildPath = path.join(__dirname, "/client/build");
+const buildPath = path.join(__dirname, "/frontend/build");
 
 app.use(express.static(buildPath))
 app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
 })
 
 app.listen(3000, () => console.log('server is listening on port 3000'));
